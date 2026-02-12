c93dd7fb8c19458b062f3d0b0ac267a78870bb8c
lsy3709 <lsy3709@naver.com>
2026-02-12 11:35:08 +0900

    3교시, 루트 컨텍스트 파일에 패키지 등록해서, 애너테이션 이용해서, 시스템에 객체 등록하고, 단위테스트로 이용하기

--- src/main/java/com/busanit501/jsp_server_project1/springex_0212/sample/_1_SampleDAO_0212.java
+++ src/main/java/com/busanit501/jsp_server_project1/springex_0212/sample/_1_SampleDAO_0212.java
@@ -1,4 +1,7 @@
 package com.busanit501.jsp_server_project1.springex_0212.sample;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class _1_SampleDAO_0212 {
 }
--- src/main/java/com/busanit501/jsp_server_project1/springex_0212/sample/_2_SampleService_0212.java
+++ src/main/java/com/busanit501/jsp_server_project1/springex_0212/sample/_2_SampleService_0212.java
@@ -1,4 +1,8 @@
 package com.busanit501.jsp_server_project1.springex_0212.sample;
 
+import org.springframework.stereotype.Service;
+
+@Service // 시스템에 등록. 어떤용도, 이 클래스가 서비스로 사용된다고,
+// 서버 시작시, 해당 객체를 활성화
 public class _2_SampleService_0212 {
 }
